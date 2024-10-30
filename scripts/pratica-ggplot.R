ddc297f2055bd10b6d01fdb99152e98388476b6b
LeticiaLopesDias <leticialssdias@outlook.com>
2024-10-30 15:14:34 -0300

    adiciona mapa

--- scripts/pratica-ggplot.R
+++ scripts/pratica-ggplot.R
@@ -37,32 +37,23 @@ dados <- read_csv("dados/sciadv.abh2932_Data_file_S1.csv",
                   locale = locale(encoding = "latin1"))
 dplyr::glimpse(dados)
 
-# Arrumar colunas
-dados <- dados |> 
-  mutate(
-    across(area_km2:loss_veg_control, as.double)
-  ) |> 
-  select(-...19)
-glimpse(dados)
-
 # Vamos entender o banco de dados:
 view(dados)
-
-
+unique(dados$gov_type)
+unique(dados$category)
 
 
 # Vamos começar nosso primeiro gráfico
 ggplot()
 
 
-# Veremos, será que a perda de vegetação está associada ao tamanho da área?
+# Explorar se a perda de vegetação está associada ao tamanho da área
 dados |> 
   ggplot() +
   geom_point(aes(x = area_km2, y = loss_veg_treatment))
 
 # Essa forma de escrita é o mesmo que:
-# g1 <- 
-  ggplot(dados) +
+ggplot(dados) +
   geom_point(aes(x = area_km2, y = loss_veg_treatment))
 
 # Ou:
@@ -70,106 +61,98 @@ ggplot(dados, aes(x = area_km2, y = loss_veg_treatment)) +
   geom_point()
 
 
-## Modificando o intervalo entre os valores do eixo (variável numérica)
-g1
-
-g1 +  
-  scale_y_continuous(labels = scales::number_format(accuracy = 0.1,
+## Ajustes estéticos 
+dados |> 
+  ggplot() +
+  geom_point(aes(x = area_km2, y = loss_veg_treatment),
+             size = 2,
+             alpha = 0.5,
+             color = "darkgreen") +
+  scale_x_continuous(labels = scales::number_format(decimal.mark = ",",
+                                                    big.mark = ".",
+                                                    scale = 1e-6,
+                                                    suffix = " M"),
+                     breaks = seq(min(dados$area_km2, na.rm = T),
+                                  max(dados$area_km2, na.rm = T),
+                                  by = 10000),
+                     expand = expansion(mult = c(0.05, 0.05))
+                     
+                     ) +
+  scale_y_continuous(labels = scales::number_format(accuracy = 0.001,
                                                     decimal.mark = ",",
-                                                    big.mark = ".")) +
-  scale_x_continuous(n.breaks = 6)
+                                                    big.mark = "."),
+                     n.breaks = 6,
+                     limits = c(-1, 1)
+                     ) +
+  labs(
+    x = "Área (km²)",
+    y = "% perda de vegetação",
+    title = "Área x Desmatamento"
+  ) +
+  theme_classic()
 
-
-# Posso definir manualmente os valores 
-
-g1 + scale_x_continuous(breaks = seq(min(dados$area_km2, na.rm = T),
-                                      max(dados$area_km2, na.rm = T),
-                                      by = 10000))
-
-
-## Formatando a unidade para "em milhões"
-
-g1 + scale_y_continuous(labels = scales::number_format(big.mark = ".",
-                                                       decimal.mark = ",",
-                                                       scale = 1e-6,
-                                                       suffix = " M"))
-
-## Modificando os limites dos eixos
-g1 + coord_cartesian(ylim = c(-1, 5000000000),
-                     xlim = c(0, 90000))
-
-g1 + scale_y_continuous(limits = c(-1, 5000000000))
-
-
-g1 + scale_x_continuous(expand = expansion(mult = c(0, 0.05)))
 
 
 ## Variáveis categóricas
-
-# g2 <- 
-  dados |> 
+dados |> 
   group_by(biome) |> 
   summarise(perda_media = mean(loss_veg_treatment, na.rm = T)) |> 
   ggplot() +
-  geom_col(aes(x = biome, y = perda_media))
-
-
-## Modificando os rótulos do eixo x (categórico)
-# Abaixo mudamos apenas o rótulo, não a ordem
-g2 + scale_x_discrete(labels = c("Amazônia",
-                                 "Mata Atlântica",
-                                 "Caatinga",
-                                 "Cerrado",
-                                 "Pampa",
-                                 "Pantanal"))
-
-
-
-## Modificando as ordens das categorias do eixo x (categórico)
-unique(dados$biome)
-
-g2 + scale_x_discrete(limits = c("Cerrado",
-                                 "Amazon",
-                                 "Pantanal",
-                                 "Atlantic Forest",
-                                  "Pampa",
-                                 "Caatinga"))
+  geom_col(aes(x = biome, y = perda_media)) +
+  # Abaixo, mudo apenas o rótulo
+  # scale_x_discrete(labels = c("Amazônia",
+  #                                "Mata Atlântica",
+  #                                "Caatinga",
+  #                                "Cerrado",
+  #                                "Pampa",
+  #                                "Pantanal")) +
+  # Dessa forma, mudo a ordem:
+  scale_x_discrete(limits = c("Cerrado",
+                              "Amazon",
+                              "Pantanal",
+                              "Atlantic Forest",
+                              "Pampa",
+                              "Caatinga"))
+  
 
 
 ## Outra opção: usando o pacote forcats (tidyverse)
 # podemos ordenar por uma variável numérica
 
-dados |> 
+# g1 <- 
+  dados |> 
   group_by(biome) |> 
   summarise(perda_media = mean(loss_veg_treatment, na.rm = T)) |> 
   mutate(biome = fct_reorder(biome, perda_media)) |> 
   ggplot() +
-  geom_col(aes(x = biome, y = perda_media)) +
-  # Modificando o nome dos eixos:
+  geom_col(aes(x = biome, y = perda_media),
+           fill = "brown") +
   labs(x = element_blank(),
-       y = "Diferença entre % \nde vegetação")
+       y = "Diferença entre % \nde vegetação") +
+  theme_classic()
+  
 
 
 ## Modificando as cores
-
-dados |> 
-  ggplot(aes(x = area_km2, y = remaining_veg_treatment_initial)) +
-  geom_point(color = "darkred") +
-  geom_smooth(method = lm, color = "grey")
-
-g3 <- dados |> 
-  ggplot(aes(x = area_km2, y = remaining_veg_treatment_initial, 
-             color = gov_type)) +
-  geom_point() +
-  geom_smooth(method = lm)
 
 # Paleta de color
 ?viridis::viridis
 viridis::viridis(4)
 
-g3 + scale_color_manual(values = c("#440154FF", "#31688EFF", "#35B779FF", "#FDE725FF"))
+# g2 <- 
+  dados |> 
+  ggplot(aes(x = area_km2, y = remaining_veg_treatment_initial, 
+             color = gov_type)) +
+  geom_point() +
+  geom_smooth(method = lm) +
+  # Inserindo valors manualmente:
+  scale_color_manual(
+    values = c("#440154FF", "#31688EFF", "#35B779FF", "#FDE725FF")
+    )
+  # Usando uma função de paleta pronta:
+  # scale_color_viridis_d(option = "magma")
+  # scale_color_npg()
 
-g3 + scale_color_viridis_d(option = "magma")
 
 # Paletas do pacote ggsci
 # Dá para usar como scale_color ou scale_fill
@@ -179,35 +162,23 @@ g3 + scale_color_viridis_d(option = "magma")
 # scale_color_jco(): Journal of Clinical Oncology color palettes
 # scale_color_tron(): This palette is inspired by the colors used in Tron Legacy. It is suitable for displaying data when using a dark theme.
 
-g3 + scale_color_npg()
-
+  
 ## Facet_wrap
 
-g3 + facet_wrap(~biome)
-
-
-## Modificando o tema
-
-g3 + theme_bw()
-g3 + theme_minimal()
-g3 + theme_classic()
-
-
-## Modificando as fontes
-# Fontes instaladas no meu notebook:
-extrafont::loadfonts(device = "win") 
-
-g3 + theme(
-  text = element_text(family = "Bookman Old Style")
-)
-
-
-## Modificando a legenda
-
-g3 + 
+dados |> 
+  ggplot(aes(x = area_km2, y = remaining_veg_treatment_initial, 
+             color = gov_type)) +
+  geom_point() +
+  geom_smooth(method = lm, se = F) +
+  scale_color_npg() +
+  facet_wrap(~biome) +
   labs(color = "Tipo de área",
        x = "Área (km²)",
        y = "Vegetação natural inicial") +
+  # Modificando o tema 
+  theme_bw() +
+  # theme_minimal() +
+  # theme_classic() +
   theme(legend.position = "bottom")
 
 
@@ -215,42 +186,69 @@ g3 +
 
 g1 + g2
 
-g1 / g3
+g1 / g2 + plot_annotation(tag_levels = 'A')
 
-(g1 + g2) / g3
+(g1 + g2) / g2
+
+
+
+## Modificando as fontes
+
+# Fontes instaladas no meu notebook:
+extrafont::loadfonts(device = "win") 
+# Importar fontes instaladas no sistema - pode levar algum tempo
+font_import()
+# Retorna vetor com os family names - usados no argumento de texto do ggplot
+fonts()
+
+loadfonts()
+
+g1 + 
+  theme(text = element_text(family = "Symbol"))
+
 
 ## Salvando a figura final em alta resolução
 
 ggsave("img/fig1.png", width = 10, height = 5, dpi = 300)
 
 
-
-## Combinando o que aprendemos até aqui:
-
-dados |> 
-  ggplot(aes(x = log(area_km2), y = remaining_veg_treatment_initial, 
-             color = gov_type)) +
-  geom_point(size = 2) +
-  geom_smooth(method = lm, se = F) + 
-  scale_y_continuous(labels = scales::number_format(accuracy = 0.1,
-                                                    decimal.mark = ","),
-                     limits = c(0,1)) +
-  labs(x = "Log(Área em km²)",
-       y = "% vegetação nativa original",
-       color = "Tipo de Governança") +
-  scale_color_npg() +
-  facet_wrap(~ biome) +
-  theme_classic() +
-  theme(legend.position = "top")
-
-
 # Caso os pontos estejam pixelados, rodar o código abaixo
-trace(grDevices::png, quote({
-  if (missing(type) && missing(antialias)) {
-    type <- "cairo-png"
-    antialias <- "subpixel"
-  }
-}), print = FALSE)
+# trace(grDevices::png, quote({
+#   if (missing(type) && missing(antialias)) {
+#     type <- "cairo-png"
+#     antialias <- "subpixel"
+#   }
+# }), print = FALSE)
 
 
-## Extra: Introdução a mapas com ggplot2
+
+## Extra: Mapas com ggplot2
+
+biomas <- geobr::read_biomes()
+biomas
+plot(st_geometry(biomas))
+
+ggplot(biomas) +
+  geom_sf(aes(fill = name_biome),
+          color = "white",
+          linewidth = 0.3
+          ) +
+  geom_sf_text(aes(label = name_biome), 
+               size = 4,
+               color = "grey90") +
+  scale_fill_viridis_d(option = "turbo") +
+  labs(x = element_blank(),
+       y = element_blank(),
+       fill = "Bioma") +
+  theme_bw() +
+  ggspatial::annotation_scale(location = 'bl', width_hint = 0.2) +
+  ggspatial::annotation_north_arrow(location = "bl", which_north = "true", 
+                                    pad_x = unit(1.5, "cm"), 
+                                    pad_y = unit(0.7, "cm"),
+                                    height = unit(0.6, "cm"),
+                                    width = unit(0.6, "cm")
+  ) +
+  theme(
+    legend.justification = "bottom",
+    panel.grid = element_blank()
+  )
